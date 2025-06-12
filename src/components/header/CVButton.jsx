4638025ecceabf1eb4aae203d1d51e7bde946ae5
Florin Marius Drilea <marius.drilea2016@gmail.com>
2025-06-12 15:33:58 +0100

    Update CVButton.jsx

--- src/components/header/CVButton.jsx
+++ src/components/header/CVButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/FlorinMarius_Drilea_CV_UK.pdf'
+import CV from '../../assets/Florin-Marius-Drilea-UK-CV.pdf'
 
 const CVButton = () => {
   return (
